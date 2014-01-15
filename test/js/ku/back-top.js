ca198da018c40014b08ce859ff4b75676dee08de
pengmiao <letbewith@163.com>
2014-01-15 23:53:51 +0800

    1
    1

---
+++ test/js/ku/back-top.js
@@ -0,0 +1,12 @@
+/*
+
+*/
+define(function(require, exports){
+    var $=require('jquery')
+
+    exports.init=function(){
+            console.log($)
+    }
+
+    
+})// end
---
+++ test/js/sea-main.js
@@ -0,0 +1,6 @@
+/*
+    主入口
+*/
+define(function(require, exports, module) {
+    	require('./ku/back-top').init()
+});
